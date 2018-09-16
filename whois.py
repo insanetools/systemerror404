b601b3ab09628dff71e7e7deac1365a4a7b2942d
insanetools <43315827+insanetools@users.noreply.github.com>
2018-09-16 19:56:58 +0700

    Update whois.py

--- whois.py
+++ whois.py
@@ -5,7 +5,7 @@ It is Auxscan 2.0 module ( WhoIs Lookup )
 from urllib2 import *
 
 print
-target = raw_input("Domain/IP Target :")
+target = raw_input("Domain/IP Target\n Input :")
 hackertarget = "http://api.hackertarget.com/whois/?q=" + target
 hackertarget = urlopen(hackertarget).read()
 print (hackertarget)
