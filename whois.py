6fa604fbbc11b6fa5ef8ceb7cb8e1087000c8845
insanetools <43315827+insanetools@users.noreply.github.com>
2018-09-16 19:50:58 +0700

    Create whois.py

---
+++ whois.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""
+It is Auxscan 2.0 module ( WhoIs Lookup )
+"""
+from urllib2 import *
+
+print
+target = raw_input("Domain/IP Target :")
+hackertarget = "http://api.hackertarget.com/whois/?q=" + target
+hackertarget = urlopen(hackertarget).read()
+print (hackertarget)
